7c33b1ad9d0f4f6c7df65854ad26119678cd4574
kimbyeonghee <55523990+kimbyeonghee@users.noreply.github.com>
2020-05-27 11:03:32 +0900

    Add files via upload

---
+++ srand.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	
+	int i;
+	srand(10); //시드(seed) 값 실행시킬 때마다 값을 바꾸고 싶을 때 = 랜덤  
+	for(i=1; i<=15; i++)
+        printf("%2d: %5d\n", i, rand());
+        
+    return 0;
+}
